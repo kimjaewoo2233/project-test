b09c097018340e7dffc74d2a998371f0b5512114
Jae woo kim <ta3844@naver.com>
2022-10-26 20:30:33 +0900

    test

---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+        @GetMapping("/api/test")
+        public String get(){
+                return "HHHHHHH";
+        }
+        @GetMapping("/api/test2")
+        public String get2(){
+                return "HHHHHHH";
+        }
+}
