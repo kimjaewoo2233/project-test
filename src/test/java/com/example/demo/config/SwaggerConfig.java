8da041df44b0efced3e7e61cf28c279f1521473d
Jae woo kim <ta3844@naver.com>
2022-10-26 19:47:15 +0900

    setting

---
+++ src/test/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@Configuration    // 스프링 실행시 설정파일 읽어드리기 위한 어노테이션
+@EnableSwagger2    // Swagger2를 사용하겠다는 어노테이션
+@SuppressWarnings("unchecked")	// warning밑줄 제거를 위한 태그
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+    //리소스 핸들러 설
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+
+}
