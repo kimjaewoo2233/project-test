d5888fcd51f58b2cbd0f423d4411034c64f05b36
김완영 <kwy@KWY.local>
2022-10-26 19:38:59 +0900

    ADD : 프론트엔드 초기설정

---
+++ src/main/frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+
+const App = () => {
+  const [test, setTest] = useState("");
+
+  useEffect(() => {
+    axios.get("api/test")
+    .then(response => setTest(response.data))
+    .catch(err => console.log(err))
+  },[]);
+  
+  return (
+    <h1>Test Data : {test}</h1>
+  )
+}
+export default App;
